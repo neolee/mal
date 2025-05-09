1a051547d576d9e08def17d1efeac7fcb7bef0dd
Neo Lee <neo.lee@gmail.com>
2025-05-09 22:46:19 +0800

    fix: param name error

--- agno/model.py
+++ agno/model.py
@@ -9,7 +9,7 @@ def model_by_provider_with_model(provider: mal.Provider, is_beta=False, model_na
     base_url = provider.beta_base_url if is_beta else provider.base_url
     model_name = model_name if model_name else provider.model_id
     return OpenAILike(
-        model=model_name,
+        id=model_name,
         api_key=provider.api_key,
         base_url=base_url
     )
