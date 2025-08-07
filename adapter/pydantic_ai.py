119e236b0619109ee86911ed4c09aeb972fc93d6
Neo Lee <neo.lee@gmail.com>
2025-08-08 00:10:11 +0800

    refactor: pydantic-ai adapter

--- adapter/pydantic_ai.py
+++ adapter/pydantic_ai.py
@@ -1,25 +1,19 @@
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
-import mal.providers as mal
+from mal.providers import provider_by_alias
+from mal.util import parse_model_str
 
 
-def openai_provider(provider: mal.Provider, is_beta=False) -> OpenAIProvider:
-    base_url = provider.beta_base_url if is_beta else provider.base_url
-    return OpenAIProvider(base_url=base_url, api_key=provider.api_key)
+def openai_provider(provider_name: str) -> OpenAIProvider:
+    provider = provider_by_alias(provider_name)
+    return OpenAIProvider(base_url=provider.base_url, api_key=provider.api_key)
 
 
-def model_by_provider_with_model(provider: mal.Provider, is_beta=False, model_name="") -> OpenAIModel:
-    op = openai_provider(provider, is_beta)
-    if not model_name: model_name = provider.model_id
-    return OpenAIModel(model_name, provider=op)
+def openai_model(model: str) -> OpenAIModel:
+    provider_name, model_id = parse_model_str(model)
+    provider = provider_by_alias(provider_name)
+    model_id = model_id if model_id else provider.model_id
 
-
-def model_by_provider(provider: mal.Provider, is_beta=False, model_type=mal.default_model_type) -> OpenAIModel:
-    model_name = provider.model_id_by_type(model_type)
-    return model_by_provider_with_model(provider, is_beta, model_name)
-
-
-def ollama_model(model_name: str) -> OpenAIModel:
-    op = openai_provider(mal.ollama_provider)
-    return OpenAIModel(model_name, provider=op)
+    op = openai_provider(provider_name)
+    return OpenAIModel(model_id, provider=op)
